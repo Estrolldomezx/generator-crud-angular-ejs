03502e85954a72269f3dbb4b4454214ba97242e5
sasiwara-sirisoft <sasiwara-sirisoft@sirisoft.com>
2023-07-24 13:59:35 +0700

    [Add] init app layout and mark comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './pages/app-layout/app-layout.component';
+// Add new imports
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppLayoutComponent,
+    canActivate: [],
+    children: [
+      {
+        path: 'app',
+        component: AppComponent,
+      },
+      // Add new components here
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+//MODULE//
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+//COMPONENT//
 import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './pages/app-layout/app-layout.component';
+//DIRECTIVE//
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+		// Add new module here
+
   ],
   providers: [],
   bootstrap: [AppComponent]
